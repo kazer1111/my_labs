4ab5c2399ab9203344b14ce43aaee6a0f27abc41
Ivanv Ivan <IvanI0101@yandex.ru>
2026-05-01 12:17:12 +0300

    Сделана лабораторная работа task3

--- src/db/backend/memory.py
+++ src/db/backend/memory.py
@@ -5,11 +5,11 @@ class StudentTable:
     def __init__(self):
         self.student = []
 
-    def create_record(self, student_id, first_name, second_name, age, sex):
+    def create_record(self, student_id,first_name,second_name,age,sex):
         if age < 0:
             raise InvalidAgeError("Возраст не может быть отрицательным")
 
-        for record in self.student:
+        for record in self.student :
             if record[0] == student_id:
                 raise DuplicateIDError("Запись с таким id уже существует")
 
@@ -21,13 +21,14 @@ class StudentTable:
             sex.strip(),
         )
         self.student.append(new_record)
+
         return new_record
 
-    def select_record(self, student_id=None, first_name=None, second_name=None, age=None, sex=None):
+    def select_record(self, student_id=None,first_name=None,second_name=None,age=None,sex=None):
         result = []
 
         for record in self.student:
-            if student_id is not None and record[0] != student_id:
+            if student_id is not None and record[0]!=student_id:
                 continue
 
             if first_name is not None and record[1] != first_name:
@@ -44,9 +45,10 @@ class StudentTable:
 
             result.append(record)
 
+        
         return result
 
-    def update_record(self, student_id, first_name=None, second_name=None, age=None, sex=None, new_id=None):
+    def update_record(self, student_id, first_name=None,second_name=None, age=None, sex=None,new_id=None):
         for i in range(len(self.student)):
             record = list(self.student[i])
 
@@ -57,7 +59,7 @@ class StudentTable:
                             raise DuplicateIDError("Запись с таким id уже существует")
                     record[0] = new_id
 
-                if first_name is not None:
+                if first_name is not None :
                     record[1] = first_name.strip()
 
                 if second_name is not None:
@@ -71,16 +73,19 @@ class StudentTable:
                 if sex is not None:
                     record[4] = sex.strip()
 
+                
                 self.student[i] = tuple(record)
                 return self.student[i]
 
         raise RecordNotFoundError("Запись не найдена")
 
     def delete_record(self, student_id):
+
         for i in range(len(self.student)):
             if self.student[i][0] == student_id:
                 deleted_record = self.student[i]
                 del self.student[i]
+
                 return deleted_record
 
         raise RecordNotFoundError("Запись не найдена")
